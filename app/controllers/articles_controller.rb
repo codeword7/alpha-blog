2e6e6cb98b42dc07f553d32952ef15e40869d8c4
Neeraj Yadav <ny29599@gmail.com>
2017-10-25 06:22:30 +0000

    Add update, delete, edit and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,16 @@
 class ArticlesController < ApplicationController
 
+    def index
+        @articles = Article.all
+    end    
+    
     def new
         @article = Article.new
-    end  
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end    
     
     def create
        # render plain: params[:article].inspect 
@@ -12,6 +20,18 @@ class ArticlesController < ApplicationController
             redirect_to article_path(@article)
         else
             render 'new'
+        end
+        
+    end
+    
+    def update
+       # render plain: params[:article].inspect 
+       @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
         end
         
     end
