e94e591dd01dd0b5b1c2afed012d20f21cb25e23
Neeraj Yadav <ny29599@gmail.com>
2017-10-27 14:59:47 +0000

    Created user table and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  resources :comments
+  resources :comments
+  resources :users
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   
@@ -40,8 +43,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
